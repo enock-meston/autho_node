fc69ca91ebfdf86896bbce0a1a11b3b0dccfca56
enock-meston <ndagijimanaenock11@gmail.com>
2024-12-01 13:35:22 +0200

    final task to view the use info

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 app.use(express.json());
@@ -23,8 +23,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((err) => console.log(err));
 
 // routes
+app.get('*/',checkUser);  // will apply this middleware to all routes
 app.get('/', (req, res) => res.render('home'));
-
 //i used middleware to check the token
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,26 +1,50 @@
-const jwt  =  require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 
-const requireAuth = (req, res, next)=>{
+const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
 
     // check json wweb token exists and is verified
-    if(token){
+    if (token) {
         //verify token
-        jwt.verify(token,'secret enock',(err,decodedToken)=>{
+        jwt.verify(token, 'secret enock', (err, decodedToken) => {
             if (err) {
                 console.log(err.message);
                 res.redirect('/login');
-            }else{
+            } else {
                 console.log(decodedToken);
                 next();
             }
         })
-    }else{
+    } else {
         // redirect to login page
         res.redirect('/login');
     }
 
 }
 
-module.exports = {requireAuth};
+
+// check current user 
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, 'secret enock', async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;  // send data the views
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth,checkUser};
